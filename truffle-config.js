0d83aebd32970887576658a71a5a4705b2f5ab8f
cheny <cheny@desktop.rich.com>
2020-04-17 11:18:38 +0800

    upgrade to solidity 0.5.0

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,17 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "192.168.111.110",
+      port: "8545",
+      network_id: "15" // match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      port: 8545,
+      network_id: 4,
+      gas: 4700000
+    }
+  }
 };
--- truffle.js
+++ truffle.js
@@ -3,9 +3,9 @@ module.exports = {
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: "7545",
-      network_id: "*" // match any network id
+      host: "192.168.111.110",
+      port: "8545",
+      network_id: "15" // match any network id
     },
     rinkeby: {
       host: "localhost",
